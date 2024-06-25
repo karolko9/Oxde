f7ec4c7665ee2e2e764e3365a96accbbb86570fb
Bolek121 <115473541+Bolek121@users.noreply.github.com>
2024-06-25 20:23:15 +0200

    1

---
+++ src/Oxde_frontend/src/components/Blog.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button @click="pobierzWpisy">refresh</button>
+        siema blog!
+        {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpisy">dodaj</button>
+    </div>
+</template>
+
+<script>
+import { Oxde_backend } from 'declarations/Oxde_backend/index';
+
+export default {
+    data() {
+        return {
+            wpisy: [],
+            nowyBlog: ""
+        }
+    },
+    methods: {
+        async dodajWpisy() {
+            await Oxde_backend.dodaj_wpis(this.nowyBlog);
+        },
+        async pobierzWpisy() {
+            this.wpisy = await Oxde_backend.odczytaj_wpisy();
+        }
+    },
+
+}
+</script>
