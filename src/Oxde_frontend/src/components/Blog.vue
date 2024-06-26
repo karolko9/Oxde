5cc5ec90f493ad0e1691e1509b995b63b247c797
Bolek121 <115473541+Bolek121@users.noreply.github.com>
2024-06-26 19:52:31 +0200

    3

--- src/Oxde_frontend/src/components/Blog.vue
+++ src/Oxde_frontend/src/components/Blog.vue
@@ -1,10 +1,20 @@
 <template>
     <div>
-        <button @click="pobierzWpisy">refresh</button>
-        siema blog!
-        {{ wpisy }}
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpisy">dodaj</button>
+        <h2 class="text-blue-600">Wpisy na bloga:</h2>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white p-4">refresh</button>
+        </div>
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="(wpis, index) in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
+                <p>id: {{ index }}</p>
+                <p>{{ wpis }}</p>
+                <button  class="bg-blue-600 rounded text-white p-4" @click="deleteWpis(index)">usun</button>
+            </div>
+        </div>
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" class="border-2 border-blue-600 p-4" type="text">
+            <button  class="bg-blue-600 rounded text-white p-4" @click="dodajWpisy">dodaj</button>
+        </div>
     </div>
 </template>
 
@@ -21,11 +31,18 @@ export default {
     methods: {
         async dodajWpisy() {
             await Oxde_backend.dodaj_wpis(this.nowyBlog);
+            await this.pobierzWpisy();
+        },
+        async deleteWpis(index) {
+            await Oxde_backend.usun_wpis(index);
+            await this.pobierzWpisy();
         },
         async pobierzWpisy() {
             this.wpisy = await Oxde_backend.odczytaj_wpisy();
         }
     },
-
+    async mounted(){
+        this.pobierzWpisy()
+    }
 }
 </script>
